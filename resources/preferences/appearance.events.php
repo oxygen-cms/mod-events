75c35c2770fb3972be949b55ecd72e338d3fa8fc
Chris Chamberlain <chris@chamberlain.id.au>
2022-03-21 10:57:12 +1100

    updated imports

--- resources/preferences/appearance.events.php
+++ resources/preferences/appearance.events.php
@@ -3,7 +3,7 @@
 use Oxygen\Preferences\Loader\PreferenceRepositoryInterface;
 use Oxygen\Preferences\Loader\DatabaseLoader;
 use Oxygen\Preferences\ThemeSpecificPreferencesFallback;
-use Oxygen\Theme\ThemeManager;
+use Oxygen\Core\Theme\ThemeManager;
 
 Preferences::register('appearance.events', function($schema) {
     $schema->setTitle('Upcoming Events');
